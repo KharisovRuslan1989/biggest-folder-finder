25e6f370bc0fd1dd5b2571627cdf8541899dc5c5
KharisovRuslan1989 <m595cb38@mail.ru>
2022-03-11 12:06:12 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args)
     {
+
+
+
         String folderPath = "C:\\Users\\user\\Desktop\\Folder";
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
+
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size + " ms");
+
+
+//        System.out.println(getFolderSize(file));
+
+        long duration = (System.currentTimeMillis()- start);
+        System.out.println(duration + " ms");
     }
 
     public static long getFolderSize(File folder)
