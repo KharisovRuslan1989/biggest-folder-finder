7a826df577d2b7e4c4a141e8b52fc64f3c7f03c3
KharisovRuslan1989 <m595cb38@mail.ru>
2022-03-11 10:44:55 +0300

    exemple1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello world");
+    public static void main(String[] args)
+    {
+        String folderPath = "C:\\Users\\user\\Desktop\\Folder";
+        File file = new File(folderPath);
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder)
+    {
+        if(folder.isFile()){
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for(File file : files)
+        {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
